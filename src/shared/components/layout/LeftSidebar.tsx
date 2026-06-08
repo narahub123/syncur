397f0eef5cc757e674688007a69c53716bfa922b
narahub123 <parknara0213@gmail.com>
2026-06-08 19:16:31 +0900

    fix: sidebar 레이아웃 및 높이 동작 수정

--- src/shared/components/layout/LeftSidebar.tsx
+++ src/shared/components/layout/LeftSidebar.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from "../common/ThemeToggle";
 
 const LeftSidebar = () => {
   return (
-    <aside className="flex h-full w-16 shrink-0 flex-col items-center border-l border-gray-100 xl:w-64">
+    <aside className="sticky top-0 flex h-screen w-16 shrink-0 flex-col items-center border-l border-gray-100 xl:w-64">
       <p>왼쪽 사이드바</p>
       <ThemeToggle />
       <div className="mt-auto">
