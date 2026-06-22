47b2acdad981917bcbdd988e4579ea6c40217111
narahub123 <parknara0213@gmail.com>
2026-06-22 13:20:43 +0900

    fix: 사이드 바 고정이 안되는 에러 수정

--- src/app/support/layout.tsx
+++ src/app/support/layout.tsx
@@ -12,7 +12,7 @@ export default async function layout({
   await requireAuth();
   return (
     <div className="mx-auto flex min-h-screen max-w-2xl xl:max-w-6xl">
-      <div className="flex h-screen w-full">
+      <div className="flex min-h-screen w-full">
         <UserSSEProvider />
         <LeftSidebar />
         <Main className="flex min-h-0 flex-1 flex-col">
